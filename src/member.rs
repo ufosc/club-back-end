063680b349143bd60fbf031949525de6134884ff
mirdaki <mirdaki@users.noreply.github.com>
2018-12-26 18:19:44 -0500

    Create add function for member table

--- src/member.rs
+++ src/member.rs
@@ -3,7 +3,8 @@ use diesel::prelude::*;
 use super::database;
 use super::schema::member;
 
-#[derive(Queryable)]
+#[derive(Insertable, Queryable)]
+#[table_name = "member"]
 pub struct Member {
 	pub ufl_username: String,
 	pub is_info_filled_out: bool,
@@ -17,11 +18,37 @@ pub struct Member {
 	pub is_in_email_list: bool,
 }
 
-pub fn test() {
+impl Member {
+	fn new(ufl_username: & str) -> Self {
+		Member {
+			ufl_username: ufl_username.to_string(),
+			..Default::default()
+		}
+	}
+}
+
+impl Default for Member {
+	fn default() -> Member {
+		Member {
+			ufl_username: "".to_string(),
+			is_info_filled_out: false,
+			first_name: "".to_string(),
+			last_name: "".to_string(),
+			github_username: "".to_string(),
+			discord_username: "".to_string(),
+			server_username: "".to_string(),
+			server_key: "".to_string(),
+			is_acm_shareable: false,
+			is_in_email_list: false,
+		}
+	}
+}
+
+pub fn list_members() {
 	let connection = database::establish_connection();
 	let results = member::table
-		.filter(member::columns::is_info_filled_out.eq(true))
-		.limit(5)
+		//.filter(member::columns::is_info_filled_out.eq(true))
+		// .limit(5)
 		.load::<Member>(&connection)
 		.expect("Error loading posts");
 
@@ -31,4 +58,15 @@ pub fn test() {
 		println!("----------\n");
 		println!("{}", member.is_info_filled_out);
 	}
+}
+
+pub fn add_member(ufl_username: &str) {
+	let connection = database::establish_connection();
+
+	let new_member = Member::new(&ufl_username);
+
+	diesel::insert_into(member::table)
+		.values(&new_member)
+		.get_result::<Member>(&connection)
+		.expect("Error saving new post");
 }
