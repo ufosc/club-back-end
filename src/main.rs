c43f8ccb6c5701f787c28b99a88898e5c01dc713
mirdaki <mirdaki@users.noreply.github.com>
2018-12-27 11:40:45 -0500

    Add test to the member module and refined member module
    - New test must be run single-threaded to ensure database accsess happens in order
    - Added comments to explain reasoning and help to organize the member module
    - Added delete function for members

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,26 @@ extern crate rocket_contrib;
 #[macro_use]
 extern crate diesel;
 
+// Utility local dependencies
 mod database;
 mod schema;
 
+// Table specifc local dependencies
 mod attendance;
 mod event;
 mod member;
 
+// Check to see if the server is working
 #[get("/")]
 fn index() -> &'static str {
 	"Hello, world!"
 }
 
+// Launch the REST server with the database connection
 fn main() {
 	rocket::ignite()
 		.attach(database::ClubDbConn::fairing())
+		// Note: Be sure to mount all the routes from differnt modules
 		.mount("/", routes![index])
 		.launch();
 }
--- src/member.rs
+++ src/member.rs
@@ -3,6 +3,9 @@ use diesel::prelude::*;
 use super::database;
 use super::schema::member;
 
+/* Struct Setup */
+
+// Struct for interacting with the member table
 #[derive(Insertable, Queryable)]
 #[table_name = "member"]
 pub struct Member {
@@ -18,8 +21,9 @@ pub struct Member {
 	pub is_in_email_list: bool,
 }
 
+// Support creating new members that supply just a UFL username
 impl Member {
-	fn new(ufl_username: & str) -> Self {
+	fn new(ufl_username: &str) -> Self {
 		Member {
 			ufl_username: ufl_username.to_string(),
 			..Default::default()
@@ -27,6 +31,8 @@ impl Member {
 	}
 }
 
+// Set default values for Member
+// Note: the UFL username should never be not set (Rust requires all values to have a default)
 impl Default for Member {
 	fn default() -> Member {
 		Member {
@@ -44,22 +50,18 @@ impl Default for Member {
 	}
 }
 
-pub fn list_members() {
+/* CRUD and other functions */
+
+// Return all members
+pub fn list_members() -> Vec<Member> {
 	let connection = database::establish_connection();
 	let results = member::table
-		//.filter(member::columns::is_info_filled_out.eq(true))
-		// .limit(5)
 		.load::<Member>(&connection)
-		.expect("Error loading posts");
-
-	println!("Displaying {} posts", results.len());
-	for member in results {
-		println!("{}", member.ufl_username);
-		println!("----------\n");
-		println!("{}", member.is_info_filled_out);
-	}
+		.expect("Error loading members");
+	results
 }
 
+// Add a member with a UFL username
 pub fn add_member(ufl_username: &str) {
 	let connection = database::establish_connection();
 
@@ -68,5 +70,68 @@ pub fn add_member(ufl_username: &str) {
 	diesel::insert_into(member::table)
 		.values(&new_member)
 		.get_result::<Member>(&connection)
-		.expect("Error saving new post");
+		.expect("Error saving new member");
+}
+
+// Remove a member by their username
+pub fn remove_member(ufl_username: &str) {
+	let connection = database::establish_connection();
+
+	let num_deleted =
+		diesel::delete(member::table.filter(member::columns::ufl_username.eq(ufl_username))) //.like(ufl_username)))
+			.execute(&connection)
+			.expect("Error deleting members");
+
+	println!("Deleted {} members", num_deleted);
+}
+
+/* Unit testing */
+
+// Note: Do run the test as `cargo test -- --test-threads=1` to run the database calls in order
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	// Utility function to clear out the whole member table
+	fn clear_table() {
+		let connection = database::establish_connection();
+
+		diesel::delete(member::table)
+			.execute(&connection)
+			.expect("Error deleting all members");
+	}
+
+	// Check to make sure no members exist by default
+	#[test]
+	fn no_members() {
+		clear_table();
+		assert_eq!(Vec::len(&list_members()), 0);
+	}
+
+	// Check that one member exists after they are created
+	#[test]
+	fn one_member() {
+		clear_table();
+		add_member("one_member_test@email.com");
+		assert_eq!(Vec::len(&list_members()), 1);
+	}
+
+	// Check that two members exist agter they are both created
+	#[test]
+	fn two_member() {
+		clear_table();
+		add_member("two_member_test_one@email.com");
+		add_member("two_member_test_two@email.com");
+		assert_eq!(Vec::len(&list_members()), 2);
+	}
+
+	// Checl that a single member can be deleted after being created
+	#[test]
+	fn delete_member() {
+		clear_table();
+		add_member("delete_member_test@email.com");
+		remove_member("delete_member_test@email.com");
+		assert_eq!(Vec::len(&list_members()), 0);
+	}
+
 }
